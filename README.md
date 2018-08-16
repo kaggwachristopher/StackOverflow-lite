b160ab27fd4a232063f1d9e8720f332e1908de61
kaggwachristopher <39350166+kaggwachristopher@users.noreply.github.com>
2018-08-16 11:19:48 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # StackOverflow-lite
 StackOverflow-lite is a platform where people can ask questions and provide answers.
+<<<<<<< HEAD
+This is the link to the site https://kaggwachristopher.github.io/StackOverflow-lite/
+=======
 
-link to live site https://kaggwachristopher.github.io
-Whatever I type will appear on the public web
+this is the link to the live site https://kaggwachristopher.github.io/StackOverflow-lite/
+>>>>>>> 4e8859c981053821eafa34c2bfb0b751b990cfd1
--- js/main.js
+++ js/main.js
@@ -1,19 +1,26 @@
-
 function myFunction() {
     document.getElementById("accountDropdown").classList.toggle("show");
 }
 
+
+function openReply() {
+    var x = document.getElementById("Reply")
+    x.onclick(function() {
+        alert("Hello");
+    })
+}
+
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+    if (!event.target.matches('.dropbtn')) {
 
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
     }
-  }
 }
